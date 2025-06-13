f3ca084a6f7dd5e6475b25adace9eea0e705ad2a
Sven Read <me@svenread.com>
2025-06-14 00:12:24 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
 const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/aeyez-report' : '';
 
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
-  basePath: isProduction ? '/aeyez-report' : '',
-  assetPrefix: isProduction ? '/aeyez-report/' : '',
-  output: 'export',  // Enable static exports for GitHub Pages
+  output: 'export',
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
 
 export default nextConfig;
